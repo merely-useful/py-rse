9b442690a85403367ca599d1985eeef4b9b353e9
Greg Wilson <gvwilson@third-bit.com>
2019-03-31 07:11:52 -0400

    Switching to 'used' syntax for referenced files

--- bin/check.py
+++ bin/check.py
@@ -224,7 +224,8 @@ def check_src(language):
         return filename[prefix_len:]
 
     content = get_all_docs(language, remove_code_blocks=False)
-    referenced = match_body(content, r'{:\s+title="([^"]+)\s*"[^}]*}')
+    referenced = match_body(content, r'{:\s+title="([^"]+)\s*"[^}]*}') | \
+        match_body(content, r'<!--\s+used="([^"]+)"\s+-->')
     actual = {_unprefix(filename)
               for filename in glob.iglob('{}/**/*.*'.format(SOURCE_DIR), recursive=True)
               if not _ignore_file(filename)}
