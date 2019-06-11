ba1c448b545485bcc70605d617cc82a4d34cac32
Jonathan Dursi <jonathan@dursi.ca>
2019-06-11 16:40:11 -0400

    Re-ordering some early material

--- src/package/04/use.py
+++ src/package/04/use.py
@@ -1,7 +1,7 @@
-import zipf
+from zipf import make_zipf, is_zipf
 
-generated = zipf.generate.make_zipf(5)
+generated = make_zipf(5)
 print('generated distribution: {}'.format(generated))
 generated[-1] *= 2
-print('passes test with default tolerance: {}'.format(zipf.is_zipf(generated)))
-print('passes test with tolerance of 1.0: {}'.format(zipf.is_zipf(generated, rel=1.0)))
+print('passes test with default tolerance: {}'.format(is_zipf(generated)))
+print('passes test with tolerance of 1.0: {}'.format(is_zipf(generated, rel=1.0)))
