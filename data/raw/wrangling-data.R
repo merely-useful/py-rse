c5ed2bbed8616b005b9fc8973f7a6f5866132f97
Luke Johnston <lwjohnst@gmail.com>
2019-07-10 00:41:25 -0400

    Start of data wrangling for co2 and dog data

--- data/raw/wrangling-data.R
+++ data/raw/wrangling-data.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(here)
+library(janitor)
 
 # NYC dog licensing dataset -----------------------------------------------
 
@@ -16,21 +17,71 @@ dog_licenses <- read_csv(
     LicenseExpiredDate = col_date("%m/%d/%Y")
   ))
 
+dog_licenses <- dog_licenses %>%
+  clean_names(case = "snake") %>%
+  rename(neighborhood_tabulation_area = nta,
+         census_tract_2010 = census_tract2010)
+
 write_csv(dog_licenses, here("data/nyc-dog-licenses.csv.gz"))
 
 # CO2 datasets ------------------------------------------------------------
+
+import_co2_data <- function(.file) {
+  .file %>%
+    read_csv(
+      skip = 57,
+      col_names = c("year", "month", NA, "date_numeric", "co2_standard",
+                    rep(NA, 5)),
+      col_types = cols_only(
+        year = "i",
+        month = "i",
+        date_numeric = "d",
+        co2_standard = "d"
+      )
+    )
+}
 
 write_lines(
   read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_in_situ_co2_mlo.csv")),
   here("data/raw/monthly-co2-mlo.csv")
 )
 
+mauna_loa_stn <- here("data/raw/monthly-co2-mlo.csv") %>%
+  import_co2_data() %>%
+  mutate(station = "Mauna Loa, Hawaii, USA")
+
 write_lines(
   read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_flask_co2_alt.csv")),
   here("data/raw/monthly-co2-alt.csv")
 )
 
+alert_stn <- here("data/raw/monthly-co2-alt.csv") %>%
+  import_co2_data() %>%
+  mutate(station = "Alert Station, NWT, Canada")
+
 write_lines(
   read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/CapeGrim_CO2_data_download.csv")),
   here("data/raw/monthly-co2-cg.csv")
 )
+
+cape_grim_stn <- here("data/raw/monthly-co2-cg.csv") %>%
+  read_csv(
+    skip = 25,
+    col_names = c("year", "month", NA, "date_numeric", "co2_standard",
+                  NA, NA),
+    col_types = cols_only(
+      year = "i",
+      month = "i",
+      date_numeric = "d",
+      co2_standard = "d"
+    )
+  ) %>%
+  mutate(station = "Cape Grim, Tasmania, Australia")
+
+co2_data <- bind_rows(
+  mauna_loa_stn,
+  alert_stn,
+  cape_grim_stn
+)
+
+write_csv(co2_data, here("data/co2.csv"))
