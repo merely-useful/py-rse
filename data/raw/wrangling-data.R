cace5475d38762ccfae3e1328df7d950c6c0056d
Luke Johnston <lwjohnst@gmail.com>
2019-07-12 11:59:00 -0400

    minor revisions to wrangling script

--- data/raw/wrangling-data.R
+++ data/raw/wrangling-data.R
@@ -41,30 +41,18 @@ import_co2_data <- function(.file) {
     )
 }
 
-write_lines(
-  read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_in_situ_co2_mlo.csv")),
-  here("data/raw/monthly-co2-mlo.csv")
-)
-
-mauna_loa_stn <- here("data/raw/monthly-co2-mlo.csv") %>%
+mauna_loa_stn <- url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_in_situ_co2_mlo.csv") %>%
+  read_lines() %>%
   import_co2_data() %>%
   mutate(station = "Mauna Loa, Hawaii, USA")
 
-write_lines(
-  read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_flask_co2_alt.csv")),
-  here("data/raw/monthly-co2-alt.csv")
-)
-
-alert_stn <- here("data/raw/monthly-co2-alt.csv") %>%
+alert_stn <- url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/monthly_flask_co2_alt.csv") %>%
+  read_lines() %>%
   import_co2_data() %>%
   mutate(station = "Alert Station, NWT, Canada")
 
-write_lines(
-  read_lines(url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/CapeGrim_CO2_data_download.csv")),
-  here("data/raw/monthly-co2-cg.csv")
-)
-
-cape_grim_stn <- here("data/raw/monthly-co2-cg.csv") %>%
+cape_grim_stn <- url("https://raw.githubusercontent.com/DamienIrving/co2_dataset/master/CapeGrim_CO2_data_download.csv") %>%
+  read_lines() %>%
   read_csv(
     skip = 25,
     col_names = c("year", "month", NA, "date_numeric", "co2_standard",
