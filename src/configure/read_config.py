955d1b32878fc67b74b7623e2b3bea6a7cc877bf
Luke Johnston <lwjohnst@gmail.com>
2019-10-05 20:29:25 +0200

    remove left over src files

--- src/configure/read_config.py
+++
@@ -1,5 +0,0 @@
-import yaml
-
-with open('config.yml', 'r') as reader:
-    config = yaml.load(reader)
-print(config)
--- src/style/looping.py
+++
@@ -1,7 +0,0 @@
-people = [
-    [['Kay', 'McNulty'], 'mcnulty@eniac.org'],
-    [['Betty', 'Jennings'], 'jennings@eniac.org'],
-    [['Marlyn', 'Wescoff'], 'mwescoff@eniac.org']
-]
-for [[first, last], email] in people:
-    print('{} {} <{}>'.format(first, last, email))
