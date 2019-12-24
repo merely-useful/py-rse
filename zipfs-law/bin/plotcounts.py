e3a31a4aea5c700f2b764c051c143e20cca0dbfe
Damien Irving <irving.damien@gmail.com>
2019-12-24 17:10:54 +1100

    Added standard input option

--- zipfs-law/bin/plotcounts.py
+++ zipfs-law/bin/plotcounts.py
@@ -7,16 +7,23 @@ import argparse
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
+mpl.rcParams['axes.labelsize'] = 'x-large'
+#mpl.rcParams['axes.titlesize'] = 'x-large'
+mpl.rcParams['xtick.labelsize'] = 'large'
+mpl.rcParams['ytick.labelsize'] = 'large'
+#mpl.rcParams['legend.fontsize'] = 'large'
 
 def main(args):
     """Run the command line program."""
 
-    df = pd.read_csv(args.infile, header=None, names=('word', 'word frequency'))
+    input_csv = args.infile if args.infile else sys.stdin
+    df = pd.read_csv(input_csv, header=None, names=('word', 'word frequency'))
     df['rank'] = df['word frequency'].rank(ascending=False)
     df.plot.scatter(x='word frequency', y='rank', loglog=True,
                     figsize=[12, 6], grid=True,
-                    xlim=[0.9, 1e4], ylim=[0.9, 1e4])
+                    xlim=args.xlim, ylim=args.ylim)
     plt.savefig(args.outfile)
 
 
@@ -25,8 +32,12 @@ if __name__ == '__main__':
     description = 'Plot word counts'
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('infile', type=str, help='Word count file')
     parser.add_argument('outfile', type=str, help='Output file (.png)')
+    parser.add_argument('--infile', type=str, default=None, help='Word count file')
+    parser.add_argument('--xlim', type=float, nargs=2, default=[0.9, 1e4],
+                        help='X-axis limits')
+    parser.add_argument('--ylim', type=float, nargs=2, default=[0.9, 1e4],
+                        help='Y-axis limits')
 
     args = parser.parse_args()
     main(args)
