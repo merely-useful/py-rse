46b4cf5cbbf822d175a7fb35b1b0b88674632825
Damien Irving <irving.damien@gmail.com>
2019-12-27 13:51:11 +1100

    Added sys.stdin to countwords.py

--- zipfs-law/bin/countwords.py
+++ zipfs-law/bin/countwords.py
@@ -23,8 +23,11 @@ def count_words(reader):
 def main(args):
     """Run the command line program."""
 
-    with open(args.infile, 'r') as reader:
-        word_counts = count_words(reader)
+    if args.infile:
+        with open(args.infile, 'r') as reader:
+            word_counts = count_words(reader)
+    else:
+        word_counts = count_words(sys.stdin)
     word_counts = mymodule.sort_counts(word_counts, args.sortby)    
     mymodule.report_results(sys.stdout, word_counts)
 
@@ -34,7 +37,8 @@ if __name__ == '__main__':
     description = 'Count the occurrence of all words in a text'
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('infile', type=str, help='Input file name')
+    parser.add_argument('--infile', type=str, default=None,
+                        help='Input file name')
     parser.add_argument('--sortby', type=str, choices=('count', 'alphabetical'),
                         default='count', help='Method for sorting results')
 
