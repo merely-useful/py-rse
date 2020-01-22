af1d2d673fb791afa2f20a7a4d24e3f0d4b85e1c
joelostblom <joel.ostblom@gmail.com>
2020-01-22 13:32:20 +0100

    Read from stdin by default

--- zipfs-law/bin/countwords.py
+++ zipfs-law/bin/countwords.py
@@ -21,19 +21,19 @@ def count_words(reader):
 
 def main(args):
     """Run the command line program."""
-    if args.infile:
+    # Read from stdin by default
+    if args.infile == sys.stdin:
+        word_counts = count_words(args.infile)
+    else:
         with open(args.infile, 'r') as reader:
             word_counts = count_words(reader)
-    else:
-        # Read from stdin if no input file is given
-        word_counts = count_words(sys.stdin)
     word_counts = mymodule.sort_counts(word_counts, args.sortby)
     mymodule.report_results(sys.stdout, word_counts)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--infile', type=str, default=None,
+    parser.add_argument('infile', type=str, default=sys.stdin, nargs='?',
                         help='Input file name')
     parser.add_argument('--sortby', type=str, choices=('count', 'alphabetical'),
                         default='count', help='Method for sorting results')
