28213df12197f8ccd8ee5a493d23b02adc04031f
joelostblom <joel.ostblom@gmail.com>
2020-01-23 15:45:07 +0100

    Change default save location to be folder independent

--- zipfs-law/bin/plotcounts.py
+++ zipfs-law/bin/plotcounts.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile', type=argparse.FileType('r'), nargs='?',
                         default='-', help='Word count csv file name')
-    parser.add_argument('--outfile', type=str, default='results/plotcounts.png',
+    parser.add_argument('--outfile', type=str, default='plotcounts.png',
                         help='Output image file name')
     parser.add_argument('--xlim', type=float, nargs=2, metavar=('XMIN', 'XMAX'),
                         default=None, help='X-axis limits')
